924618841fbaab15d9181fce94990f194655ec04
saivenkatsharma <saivenkatsharma3@gmail.com>
2024-10-04 11:19:55 +0530

    fix : "new folder"

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    setTimeout(function() {
+        const loadingContainer = document.getElementById('loading-container');
+        loadingContainer.style.display = 'none'; 
+    }, 5000); 
+};
